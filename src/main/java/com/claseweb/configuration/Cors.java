43b4b3a56fac5b4868966b80e025fe58da74b90d
Alejandro Uscátegui <uscateguit@gmail.com>
2023-11-14 19:50:07 -0500

    Pequeña correccion

--- src/main/java/com/claseweb/configuration/Cors.java
+++ src/main/java/com/claseweb/configuration/Cors.java
@@ -1,9 +1,13 @@
 package com.claseweb.configuration;
 
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 public class Cors implements WebMvcConfigurer {
 
     @Override
--- src/main/java/com/claseweb/entidades/Libro.java
+++ src/main/java/com/claseweb/entidades/Libro.java
@@ -6,7 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+@Entity (name = "libroUscategui")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
